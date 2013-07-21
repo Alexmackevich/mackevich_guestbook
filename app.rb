da0be54f8814cb1b0e6522036a43d000d4c72103
Alex <41exmackevich@gmail.com>
2013-07-22 00:27:09 +0300

    guestbook without bootstrap

--- app.rb
+++ app.rb
@@ -1,29 +1,66 @@
 require 'sinatra'
 
-get '/' do
-	erb :index
-end
 
-post '/' do 
-	author = params[:author].strip.chomp
-	content = params[:content].strip.chomp
-	write_post(author, content)
+
+
+get '/' do
+  @posts = read_posts
   erb :index
 end
 
+get '/clear' do
+  clear_file
 
-def read_posts
-	file_content = File.open("posts.txt") do |file|
-		file.read
-	end
-
+  redirect to('/')
 end
 
+post '/' do
+  author = params[:author]
+  content = params[:content]
+  if author.empty? != true && content.empty? != true
+    write_post author, content
+end
+
+erb :index
+redirect to('/')
+end
+
+
+
+def read_posts
+  file_content = File.open("posts.txt") do |file|
+    file.read
+  end
+
+  rows = file_content.split("=====\n").map{ |row| row.chomp }
+
+  paragraphs = rows.collect do |row|
+    if /^date: (?<date>.*)\nname: (?<name>.*)\ncontent: (?<content>.*)$/m =~ row
+      { date:date, name: name, content: content }
+    end
+  end
+
+  line = ""
+  paragraphs.each do |paragraph|
+    line << "<p>Date: #{paragraph[:date]}<br> Name: #{paragraph[:name]}<br> 
+                                  Post: #{paragraph[:content]}<br></br> "
+  
+  end 
+    line
+end
+
+
 def write_post (author, content)
-	File.open('posts.txt', 'a') do |file|
-		file.puts "date:#{Time.new}"
-		file.puts "author:#{author}"
-		file.puts "content:#{content}"
-		file.puts "====="
-	end
+  File.open('posts.txt', 'a') do |file|
+    file.puts "date: #{Time.new}"
+    file.puts "name: #{author}"
+    file.puts "content: #{content}"
+    file.puts "====="
+  end
+end
+
+def clear_file
+  File.open('posts.txt', 'w') do |file| 
+    file.print ''
+  end
 end
